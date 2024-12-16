325a4aec7dfecae9600669c78c78d10a95d5245f
Andrew Barker <andrewbarker96@gmail.com>
2024-12-16 08:07:59 -0600

    -

--- firebase/index.js
+++ firebase/index.js
@@ -12,14 +12,14 @@ config();
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCGc9ieWeevCMiMPBDv3VEYuedyK2Xe2jI",
-  authDomain: "stockassoc-a0091.firebaseapp.com",
-  databaseURL: "https://stockassoc-a0091-default-rtdb.firebaseio.com",
-  projectId: "stockassoc-a0091",
-  storageBucket: "stockassoc-a0091.appspot.com",
-  messagingSenderId: "1080703289236",
-  appId: "1:1080703289236:web:a13f5bec71795e62083181",
-  measurementId: "G-3MHPJHP7BR",
+  apiKey: process.env.FIREBASE_API,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_URL,
+  projectId: process.env.FIREBASE_ID,
+  storageBucket: process.env.FIREBASE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGE_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
